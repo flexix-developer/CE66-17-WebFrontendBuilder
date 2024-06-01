d5bc0d6fec68afd0f0fb6343d3fd9d4df0e9b9be
Mon Aekarin <Aekarin.Ongart@gmail.com>
2024-06-02 04:20:48 +0700

    upload doc

--- backend/controller/workspace/workspace.go
+++
@@ -1,8 +0,0 @@
-package workspace
-
-type workspacebody struct {
-	Fname string `json:"fname" validate:"require"`
-	Lname string `json:"lname" validate:"require"`
-	Email string `json:"email" validate:"require"`
-	Pass  string `json:"pass" validate:"require"`
-}
--- backend/user_project_path/4/6/scripts.js
+++
@@ -1,8 +0,0 @@
-// เลือกทุก <a> ในเอกสาร
- var allLinks = document.querySelectorAll("a");
-
- // วนลูปผ่านทุก <a> เพื่อลบคำสั่ง onClick
- allLinks.forEach(function (link) {
-   // ลบคำสั่ง onClick ออก
-   link.removeAttribute("onClick");
- });
